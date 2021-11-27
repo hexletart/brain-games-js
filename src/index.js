94dc815393ce6b9085b0047022825e310c74da27
Art Hexlet <hexlet_art@mail.ru>
2021-11-27 17:53:01 +0300

    introduce new engine in package and write it

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import readlineSync from 'readline-sync';
+import greeting from './cli.js';
+
+export default (descr, gameData) => {
+  const name = greeting();
+  console.log(descr);
+  for (const roundData of gameData) {
+    console.log(`Question: ${roundData[0]}`);
+    const answer = readlineSync.question('Your answer: ');
+    if (answer !== roundData[1]) {
+      const failed = `'${answer}' is wrong answer ;(. Correct answer was '${roundData[1]}'.
+Let's try again, ${name}!`;
+      return console.log(failed);
+    }
+  }
+  return console.log(`Congratulations, ${name}!`);
+};
