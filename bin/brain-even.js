b54bb0f9bd38304211d3f9de91e26f77ebede430
Art Hexlet <hexlet_art@mail.ru>
2021-11-27 17:58:23 +0300

    rebuild the file because of index.js

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
-import greeting from '../src/cli.js';
-import checkParity from '../src/is-even.js';
+import checkParity from '../src/games/is-even.js';
 
-checkParity(greeting());
+checkParity();
