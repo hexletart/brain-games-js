d2efe6086f6f3d707b97cbda18dfb14f59b38b7f
Art Hexlet <hexlet_art@mail.ru>
2021-11-30 09:05:06 +0300

    add getRandomInRange function

--- src/random-items.js
+++ src/random-items.js
@@ -1,5 +1,8 @@
 export const randomNum = (border) => Math.floor(Math.random() * (border * 2) - border);
+
 export const randomPlusNum = (border) => Math.floor(Math.random() * border);
+
+export const getRandomInRange = (min, max) => Math.floor(Math.random() * (max - min)) + min;
 
 export const randomOper = () => {
   const operators = ['+', '-', '*'];
